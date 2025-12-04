0321f9937e19ddc4735fa8625c4a5daa32b5236a
mattis_tjn <152501338+mattistjnn@users.noreply.github.com>
2025-12-04 17:24:44 +0100

    Modification README avec ajout de liens direct pour les suivis de projet

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # mspr-test
+
+Documentation projet wiki :
+
+Doc avec photos et idées : https://docs.google.com/document/d/1jjHL4_lxWkgASEeRzizDPcpEi6OhKX5dK6hsRjcNU00/edit?tab=t.0
+
+Trello pour suivi des taches : https://trello.com/b/w48aXOCH/mspr-ntl-systoolbox
--- modules/diagnostic.py
+++ modules/diagnostic.py
@@ -5,3 +5,30 @@
 #           Permettre de vérifier la version d’OS, l’uptime, l’utilisation des ressources CPU / RAM / Disques pour une machine Windows Server
 #           Permettre de vérifier la version d’OS, l’uptime, l’utilisation des ressources CPU / RAM / Disques pour une machine Ubuntu
 
+
+import psutil
+import platform
+import time
+import json
+
+def check_system_resources():
+    """
+    Vérifie OS, Uptime, CPU, RAM, Disque.
+    Retourne un dictionnaire (compatible JSON).
+    """ 
+    boot_time = psutil.boot_time()
+    uptime_seconds = time.time() - boot_time    
+    
+    disk_usage = psutil.disk_usage('/') 
+    
+    data = {
+        "os_version": platform.platform(),
+        "uptime_hours": round(uptime_seconds / 3600, 2),
+        "cpu_percent": psutil.cpu_percent(interval=1),
+        "ram_percent": psutil.virtual_memory().percent,
+        "disk_percent": disk_usage.percent
+    }
+    return data
+
+
+print(check_system_resources())
