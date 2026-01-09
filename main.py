998fd091283c1cdabf16ec4c68e60f4d424cb49b
mattis_tjn <152501338+mattistjnn@users.noreply.github.com>
2026-01-09 14:00:45 +0100

    ajout nouvelle achine

--- main.py
+++ main.py
@@ -17,7 +17,9 @@ INFRA = {
     "wms-db": {"ip": "192.168.10.21", "user": "wms-db", "pwd": "passroot", "os": "ubuntu"},
     "wms-app": {"ip": "192.168.10.22", "user": "wms-app", "pwd": "passroot", "os": "ubuntu"},
     "DC01": {"ip": "192.168.10.10", "user": "Administrateur@nord-transit.fr", "pwd": "caca31000!", "os": "windows"},
-    "DC02": {"ip": "192.168.10.11", "user": "Administrateur@nord-transit.fr", "pwd": "caca31000!", "os": "windows"}
+    "DC02": {"ip": "192.168.10.11", "user": "Administrateur@nord-transit.fr", "pwd": "caca31000!", "os": "windows"},
+    "IPBX-VM": {"ip": "192.168.10.40", "user": "ipbx", "pwd": "passipbx", "os": "ubuntu"}, # CentOS est géré comme Ubuntu/Linux ici
+    "SUPER-01": {"ip": "192.168.10.50", "user": "Administrateur@nord-transit.fr", "pwd": "caca31000!", "os": "windows"}
 }
 def get_timestamp():
     """Génère un horodatage ISO pour les rapports."""
